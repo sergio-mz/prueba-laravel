c6cdae69032904f79a06f3ab670f7fcd0e85a81a
sergio-mz <139819110+sergio-mz@users.noreply.github.com>
2023-07-27 17:14:43 -0500

    Eloquent & Relatioships

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -13,20 +14,19 @@ class PageController extends Controller
 
     public function blog()
     {
-        //consulta a base de datos
-        $posts = [
-            ['id' => 1, 'title' => 'PHP',     'slug' => 'php'],
-            ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
-        ];
+        /* $posts = Post::get(); */
+        // $post = Post::first();
+        // $post = Post::find(25);
+
+        //dd($post);
+
+        $posts = Post::latest()->paginate();
 
         return view('blog', ['posts' => $posts]);
     }
 
-    public function post($slug)
+    public function post(Post $post)
     {
-       //consulta a base de datos
-        $post = $slug;
-
         return view('post', ['post' => $post]); 
     }
 }
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,11 +6,14 @@
 
 @foreach ($posts as $post)
 <p>
-    <strong>{{ $post['id'] }}</strong>
-    <a href="{{ route('post', $post['slug']) }}">
-        {{ $post['title'] }}
+    <strong>{{ $post->id }}</strong>
+    <a href="{{ route('post', $post->slug) }}">
+        {{ $post->title }}
     </a>
+    <br>
+    <span>{{$post->user->name}}</span>
 </p>
 @endforeach
 
+{{ $posts->links() }}
 @endsection
